70b49da24bc867878d6e0d66b2e78c9124309253
Felix Zhu <zhuhangm@gmail.com>
2013-10-04 07:51:01 +0200

    added spreadsheet

--- app/models/concerns/visitor.rb
+++
@@ -1,8 +0,0 @@
-class Visitor < ActiveRecord::Base
-	has_no_table
-	column :favorite, :string
-	column :comment, :string
-	validates_presence_of :favorite
-
-	IMAGE_LABELS = ['San Francisco','Sydney','Paris']
-end
